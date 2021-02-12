2a0b95e2d39e12ca5c13e3647bf3491cce0f83db
maxnth <maximilian.noeth@protonmail.com>
2021-02-12 14:22:18 +0100

    update project config files

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='ocr4all_helpers',
       version='0.3.1',
       description='Different python scripts used in the OCR4all workflow.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/OCR4all/OCR4all_helper-scripts',
       author='Nico Balbach, Maximilian Nöth',
       author_email='nico.balbach@informatik.uni-wuerzburg.de, maximilian.noeth@protonmail.com',
       packages=find_packages(),
-      license='GPL-v3.0',
+      license='MIT License',
+      classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+          ],
       entry_points={
             'console_scripts': [
                   'pagelineseg=ocr4all_helpers.pagelineseg:cli',
@@ -17,4 +27,6 @@ setup(name='ocr4all_helpers',
             ],
       },
       install_requires=open("requirements.txt").read().split(),
-      zip_safe=False)
+      python_requires='>=3.6',
+      zip_safe=False
+      )
