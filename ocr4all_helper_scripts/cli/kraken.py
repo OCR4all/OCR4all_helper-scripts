b51532dce31bd92ec4e9dfa1f8172d262730feef
maxnth <maximilian.noeth@protonmail.com>
2021-11-02 16:22:33 +0100

    fix kraken cli name

--- ocr4all_helper_scripts/cli/kraken.py
+++ ocr4all_helper_scripts/cli/kraken.py
@@ -4,7 +4,7 @@ from ocr4all_helper_scripts.helpers import kraken_helper
 import click
 
 
-@click.command("sync-text-equiv", help="Sync region text equiv elements with textline text equiv content.")
+@click.command("kraken", help="Sync region text equiv elements with textline text equiv content.")
 @click.argument("FILES", nargs=-1, required=True, type=str)
 def kraken_cli(files):
     helper = kraken_helper.KrakenHelper(files)
