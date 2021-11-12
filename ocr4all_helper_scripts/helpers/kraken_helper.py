f2f227c3235f6292fcef8abff0554357a2eb2830
maxnth <maximilian.noeth@protonmail.com>
2021-11-12 16:36:07 +0100

    fix false Element name in xpath

--- ocr4all_helper_scripts/helpers/kraken_helper.py
+++ ocr4all_helper_scripts/helpers/kraken_helper.py
@@ -51,10 +51,10 @@ class KrakenHelper:
 
     @staticmethod
     def shrink_full_page_region(text_region: etree.Element):
-        region_coord = text_region.find("./{*}Coord")
+        region_coord = text_region.find("./{*}Coords")
 
         textline = text_region.find("./{*}TextLine")
-        textline_coord = textline.find("./{*}Coord")
+        textline_coord = textline.find("./{*}Coords")
 
         region_coord.set("points", textline_coord.get("points"))
 
